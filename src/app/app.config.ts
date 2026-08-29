5c752dd2df92d9ccfb9d57c493d6e5d2088dfebe
Sahid Fakiry <s.fakiry23@gmail.com>
2026-08-29 22:03:45 +0200

    feat(medilog): primeng ui lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+      license: environment.primeUILicense,
+    }),
+  ],
 };
