73b03f2a6add7f58254bc46ef879b60c71559f4d
Sahid Fakiry <s.fakiry23@gmail.com>
2026-09-05 14:19:32 +0200

    feat(medilog): add cards for the created sups/meds

--- src/app/pill-log/pill-log.ts
+++ src/app/pill-log/pill-log.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { ButtonDirective } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
 import { LOCAL_STORAGE_KEY, PillModel } from './pill-log.models';
 import { PillEditor } from '../pill-editor/pill-editor';
+import { CaretDown } from '@primeicons/angular/caret-down';
+import { CaretUp } from '@primeicons/angular/caret-up';
+import { Refresh } from '@primeicons/angular/refresh';
 
 @Component({
-  imports: [DialogModule, ButtonDirective, PillEditor],
+  imports: [DialogModule, ButtonDirective, PillEditor, CardModule, CaretUp, CaretDown, Refresh],
   selector: 'app-pill-log',
   styleUrl: './pill-log.css',
   templateUrl: './pill-log.html',
@@ -17,14 +21,20 @@ export class PillLog {
 
   private readonly defaultPillModel: PillModel = { name: '', goal: 0, id: 0, intake: 0 };
 
-  openDialog(): void {
-    this.visible.set(true);
-    try {
-      const pills = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '');
-      if (pills) this.pillsToCreate.set(pills);
-    } catch (error) {
-      console.warn('No Entries found.');
+  constructor() {
+    const pills = this.retrievePills();
+    if (pills && pills.length) {
+      this.pillsToCreate.set(pills);
+      this.pills.set(pills);
     }
+  }
+
+  openDialog(): void {
+    const pills = this.retrievePills();
+    if (pills && pills.length) {
+      this.pillsToCreate.set(pills);
+    }
+    this.visible.set(true);
   }
 
   closeDialog(): void {
@@ -41,8 +51,61 @@ export class PillLog {
   }
 
   savePills(): void {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.pillsToCreate()));
+    this.setToLocalStorage(this.pillsToCreate());
     this.pills.set(this.pillsToCreate());
     this.closeDialog();
+  }
+
+  incrementIntake(index: number): void {
+    this.pills.update((values) => {
+      values[index].intake++;
+      return values;
+    });
+
+    this.setToLocalStorage(this.pills());
+  }
+
+  decrementIntake(index: number): void {
+    this.pills.update((values) => {
+      values[index].intake--;
+      return values;
+    });
+
+    this.setToLocalStorage(this.pills());
+  }
+
+  resetIntake(index: number): void {
+    this.pills.update((values) => {
+      values[index].intake = 0;
+      return values;
+    });
+
+    this.setToLocalStorage(this.pills());
+  }
+
+  resetAll(): void {
+    this.pills.update((values) => {
+      values.map((value) => (value.intake = 0));
+      return values;
+    });
+
+    this.setToLocalStorage(this.pills());
+  }
+
+  private setToLocalStorage(pills: PillModel[]): void {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(pills));
+  }
+
+  private retrievePills(): PillModel[] | undefined {
+    try {
+      const pills = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '');
+      if (pills) {
+        return pills as PillModel[];
+      }
+      return undefined;
+    } catch (error) {
+      console.warn('No Entries found.');
+      return undefined;
+    }
   }
 }
